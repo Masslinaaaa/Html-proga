1580a6cb2477fdfaf7dd80bc1f1dde20d0a11cc0
Masslinaaaa <83066524+Masslinaaaa@users.noreply.github.com>
2021-04-23 12:43:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,127 @@
+const usr = 12
+console.log(usr)
+const string = 'green'
+console.log(string)
+const ban = null
+console.log(ban)
+const sin = true
+console.log(sin)
+const tipe = undefined
+console.log(tipe)
+const variable = Symbol()
+console.log(variable)
+const person = {name : 'Masslina' , age: 21}
+console.log(person)
+/*alert("???")
+let burn = prompt ("Буду ли я сегодня нормально спать?", "yes")
+let ay = confirm("Вы примите мои лабы?")
+let age = prompt ("Сколько вам лет?")
+console.log ("Ваш возвраст:" +age)*/
+
+/*let food = prompt ("Сколько у вас купленых товаров?")
+let sereal = prompt ("Какова цена за один товар?")
+let all = 0
+all = food * sereal
+console.log ("Сумма равна:" +all)*/
+
+/*let num = prompt ("Введите число")
+if (num < 0) {alert ("Отрицательное")}
+else
+{alert ("Положительное")}*/
+
+/*let week = + prompt ("Выберите день недели?")
+switch(week) {
+    case 1:
+        alert( 'Понедельник')
+        break
+    case 2:
+        alert( 'Фторник')
+        break
+    case 3:
+        alert( 'Среда')
+        break
+    case 4:
+        alert( 'Четверг')
+        break
+    case 5:
+        alert( 'Пятница')
+        break
+    case 6:
+        alert( 'Суббота')
+        break
+    case 7:
+        alert( 'Воскресение')
+        break
+    default:
+        alert("Такого нет!")}*/
+
+/*for (let i = 1; i<=10; i++)
+{
+    console.log(`6*${i}=${6*i}`)
+    }*/
+
+/*const array = [1,2,3,4,5,-7,-8]
+console.log (Math.pow(array[2],2))
+const boge = array[0] + array[array.length-1]
+console.log (boge)
+let result = 0
+for (let n = 0; n<=array.length-1; n++)
+{
+    if (array[n] < 0) {result +=(array[n]*array[n])
+    }
+}
+console.log (result)*/
+
+/*const car = {name: "Lada",model: "guano",engine_hoop: 57}
+const car1 = {name: "Boll",model: "Tayota",engine_hoop: 38}
+const car2 = {name: "Nell",model: "Nisan",engine_hoop: 167}
+const array1 = [car,car1,car2]
+let sick = 0
+for (let n = 0; n <array1.length; n++)
+{
+    if (array1[n].engine_hoop < array1[sick].engine_hoop){
+    }
+
+}
+console.log (array1[sick])
+ */
+
+/*let e = 0
+let f = 0
+let g = 0
+let a = prompt ("Введите число")
+console.log ("a:" +a)
+let b = prompt ("Введите число")
+console.log ("b:" +b)
+let c = prompt ("Введите число")
+console.log ("c:" +c)
+let d = prompt ("Введите число")
+console.log ("d:" +d)
+if (a<b) {
+    e = a
+}
+else {
+    e = b
+}
+if (c<d) {
+    f = c
+}
+else {
+    f = d
+}
+if  (e > f){
+    g = e
+}
+else {
+    g = f
+}
+console.log ("Максимвльное число:" +g)
+ */
+
+const letter = 'Masslina'
+console.log (letter.length)
+const reversetletter = letter.split ('').reverse().join('')
+console.log (reversetletter)
+
+
+
